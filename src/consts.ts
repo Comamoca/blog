641e667ad63f9c0c11a54b9a2f04243b71532f67
Comamoca <comamoca.dev@gmail.com>
2023-05-20 14:11:16 +0900

    :sparkles: (index.astro) change site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "こまぶろぐ";
+export const SITE_TITLE = "🧠 |> IO.puts()";
 export const SITE_DESCRIPTION =
   "テクニカルな事から晩ごはんの事まで書き連ねるブログ";
