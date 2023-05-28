836cc584b50713f366b7332ab694d5b44450ce3b
Comamoca <comamoca.dev@gmail.com>
2023-05-28 23:47:54 +0900

    :sparkles: (Logo.tsx) title of the site was cut out of the component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+export default function Logo() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <img src="https://emoji2svg.deno.dev/api/🧠" class="w-10 mt-2.5 mr-1" />
+        <h1 class="text-3xl mt-2 md:text-4xl font-body text-center">
+          |&gt; IO.puts()
+        </h1>
+      </div>
+    </>
+  );
+}
