c6b6530849c327faeadb14802b38d864f324bcf8
Comamoca <comamoca.dev@gmail.com>
2024-04-01 01:32:12 +0900

    :tada: (global.css) Add link card.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import rlc from "remark-link-card";
 
 import tailwind from "@astrojs/tailwind";
 import { remarkReadingTime } from "./src/utils";
@@ -9,13 +10,17 @@ import { remarkReadingTime } from "./src/utils";
 export default defineConfig({
   site: "https://comamoca.pages.dev",
   integrations: [
-    mdx({ remarkPlugins: [remarkReadingTime] }),
+    mdx({
+      remarkPlugins: [
+        remarkReadingTime,
+      ],
+    }),
     sitemap(),
     react(),
     tailwind(),
   ],
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime, rlc],
     shikiConfig: {
       theme: "solarized-light",
       wrap: false,
