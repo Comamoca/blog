ffc4db331d02ef8377c1cb93b7c5e9be78a56634
Comamoca <comamoca.dev@gmail.com>
2024-05-18 02:39:11 +0900

    :sparkles: (Logo.tsx) Add alt to img tag.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <>
       <div className="flex justify-center">
-        <img src="https://emoji2svg.deno.dev/api/🧠" class="w-10 mt-2.5 mr-1" />
+        <img src="https://emoji2svg.deno.dev/api/🧠" class="w-10 mt-2.5 mr-1" alt="brain" />
         <h1 class="text-3xl mt-2 md:text-4xl font-body text-center">
           |&gt; IO.puts()
         </h1>
