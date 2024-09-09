e1e0f0a950a7295cc6870161a803af7c678b7ea3
Comamoca <comamoca.dev@gmail.com>
2024-09-09 12:38:59 +0900

    :beetle: (pages/index.astro, components/PostCard.astro) Chromeでの表示の問題を修正
    Chromeで見出しが白く表示されてしまう問題を修正。これはChromeがデフォルトでダークモードを使う関係で、削除し忘れていた`dark:`クラスが有効化されてしまうというのが原因でした。なのでそれらのクラスを削除し問題を修正しました。
    
    Closes #1

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -11,7 +11,8 @@ export default function PostCard(props: { post }) {
         className="block p-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow md:p-4 md:p-6 hover:bg-gray-100"
         title={data.title}
       >
-        <h3 className="mb-2 text-lg font-bold tracking-tight text-gray-900 md:text-xl md:text-2xl dark:text-white">
+      {/* dark:text-white */}
+        <h3 className="mb-2 text-lg font-bold tracking-tight text-gray-900 md:text-xl md:text-2xl"> 
           {data.title}
         </h3>
         <p className="font-normal text-gray-700">
