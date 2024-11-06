d11b22256363ee34819c51ced7f63344a34972cd
Comamoca <comamoca.dev@gmail.com>
2024-11-07 08:53:01 +0900

    :seedling: (Init)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "code": {
+              // color: "#f87171",
+		    // borderRadius: '0.25rem',
+            },
+            "code::after": {
+              content: "none",
+            },
+            "code::before": {
+              content: "none",
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require("@tailwindcss/typography")],
 };
