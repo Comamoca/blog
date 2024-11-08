d52ee4cfd4f964b9bbd3d83a17e2749b599d63e2
Comamoca <comamoca.dev@gmail.com>
2024-11-09 00:20:01 +0900

    :sparkles: (PostCard.tsx) Update style

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -5,21 +5,19 @@ export default function PostCard(props) {
 
   const { title, description, slug } = props;
 
-  // console.log(description)
-
   return (
     <div className="border(t gray-200) w-70 md:w-100 m-3 p-4">
       <a
         href={slug}
-        className="block p-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow md:p-4 md:p-6 hover:bg-gray-100"
+        className="block p-3 mx-20 bg-white rounded-lg border border-gray-200 shadow md:p-6 hover:bg-gray-100"
         title={title}
       >
         {/* dark:text-white */}
-        <h3 className="mb-2 text-lg font-bold tracking-tight text-gray-900 md:text-xl md:text-2xl">
+        <h3 className="mb-2 text-lg font-bold tracking-tight text-gray-900 md:text-2xl">
           {title}
         </h3>
         <p className="font-normal text-gray-700">
-          {`${description}...`}
+          {description}
         </p>
       </a>
     </div>
