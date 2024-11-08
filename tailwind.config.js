6d782c58c8aebb454e1d7aaae85c612da47a563a
Comamoca <comamoca.dev@gmail.com>
2024-11-08 16:54:24 +0900

    :tada: (tailwind.config.js) Add Tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import typography from "npm:@tailwindcss/typography";
+import daisyui from "npm:daisyui@latest";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography, daisyui],
+};
