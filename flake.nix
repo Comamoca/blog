e0e2328a8b740db8cbb25ba6564543bfd5f2b6b7
Comamoca <comamoca.dev@gmail.com>
2024-11-10 00:49:00 +0900

    :package: (flake) Add git-hooks

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     treefmt-nix.url = "github:numtide/treefmt-nix";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
   };
 
   outputs =
@@ -15,9 +16,13 @@
       nixpkgs,
       treefmt-nix,
       flake-parts,
+      git-hooks-nix,
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ treefmt-nix.flakeModule ];
+      imports = [
+        treefmt-nix.flakeModule
+        inputs.git-hooks-nix.flakeModule
+      ];
       systems = import inputs.systems;
 
       perSystem =
@@ -46,20 +51,8 @@
               };
             };
           };
-        in {
-          treefmt = {
-            projectRootFile = "flake.nix";
-            programs = {
-              nixfmt.enable = true;
-              deno.enable = true;
-              rufo.enable = true;
-            };
 
-            settings.formatter = { };
-          };
-
-          # When execute `nix develop`, you go in shell installed nil.
-          devShells.default = pkgs.mkShell {
+	  devShell = pkgs.mkShell {
             packages = with pkgs; [
               (textlint.withPackages [
                 textlint-rule-preset-ja-technical-writing
@@ -78,14 +71,48 @@
 
               just
               nushell
-	      git-secrets
+              git-secrets
             ];
 
-          shellHook = ''
+            shellHook = ''
               unlink .textlintrc
               ln -s ${textlintrc} .textlintrc
-          '';
+            '';
           };
+        in
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              deno.enable = true;
+              rufo.enable = true;
+            };
+
+            settings.formatter = { };
+          };
+
+	  pre-commit = {
+	    check.enable = true;
+	    devShell = devShell;
+	    settings = {
+	      repos = [
+	        {
+	          repo = "local";
+		  hooks = {
+		    id = "git-secrets";
+		    name = "git secrets";
+                    entry = "git secrets --scan";
+                    language = "system";
+                    types = ["text"];
+		  };
+	        }
+	      ];
+	    };
+	  };
+
+          # When execute `nix develop`, you go in shell installed nil.
+          devShells.default = devShell;
         };
     };
 }
