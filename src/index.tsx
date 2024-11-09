1c218fcf724b645aa06864e4d2fecb2a5c3fd9eb
Comamoca <comamoca.dev@gmail.com>
2024-11-09 21:54:54 +0900

    :sparkles: (index.tsx) Update index page

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,40 @@
 import { SITE_TITLE } from "./consts.ts";
 import { basename } from "jsr:@std/path";
 import PostCard from "./components/PostCard.tsx";
+import Logo from "./components/Logo.tsx";
 
 export const layout = "layouts/main.tsx";
 
 export default async function ({ search }: Lume.Data, helpers: Lume.Helpers) {
   return (
     <>
-      {search.pages("posts")
-        .reverse()
-        .filter((page) => basename(page.url).indexOf("-diary") == -1)
-        .map((page, idx) => {
-          return (
-            <PostCard
-              key={idx}
-              title={page.title}
-              slug={page.url}
-              description={page.description}
-            />
-          );
-        })}
+      <div className="-mt-20 mb-10">
+        <Logo />
+      </div>
+      <div className="flex flex-col">
+        {search.pages("posts")
+          .filter((page) => basename(page.url).indexOf("-diary") == -1)
+          .sort((a, b) => {
+            // Sort articles by `pubDate`
+
+            const dateA = new Date(a.pubDate);
+            const dateB = new Date(b.pubDate);
+
+            return (dateA - dateB) * -1;
+          })
+          .map((page, idx) => {
+            return (
+              <div className="mx-auto justify-center">
+                <PostCard
+                  key={idx}
+                  title={page.title}
+                  slug={page.url}
+                  description={page.description}
+                />
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 }
