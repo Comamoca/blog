ad58b88fbd5fafc9c54a566c356d4ef8fba85f9f
Comamoca <comamoca.dev@gmail.com>
2024-11-10 00:07:12 +0900

    :package: (flake) Switch to flake-parts from flake-utils

--- flake.nix
+++ flake.nix
@@ -1,76 +1,91 @@
 {
-  description = "A very basic flake";
+  description = "A basic flake to with flake-parts";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
   };
-  # crate2nix ={};
 
   outputs =
-    {
+    inputs@{
       self,
+      systems,
       nixpkgs,
-      flake-utils,
       treefmt-nix,
-      ...
+      flake-parts,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        stdenv = pkgs.stdenv;
-        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ treefmt-nix.flakeModule ];
+      systems = import inputs.systems;
 
-        textlintrc = (pkgs.formats.json { }).generate "textlintrc" {
-          filters = { };
-          rules = {
-            preset-ja-technical-writing = {
-              ja-no-weak-phrase = false;
-              ja-no-mixed-period = false;
-              no-exclamation-question-mark = false;
-            };
-            prh = {
-              rulePaths = [
-                "${pkgs.textlint-rule-prh}/lib/node_modules/textlint-rule-prh/node_modules/prh/prh-rules/media/WEB+DB_PRESS.yml"
-                "${pkgs.textlint-rule-prh}/lib/node_modules/textlint-rule-prh/node_modules/prh/prh-rules/media/techbooster.yml"
-              ];
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          stdenv = pkgs.stdenv;
+
+          textlintrc = (pkgs.formats.json { }).generate "textlintrc" {
+            filters = { };
+            rules = {
+              preset-ja-technical-writing = {
+                ja-no-weak-phrase = false;
+                ja-no-mixed-period = false;
+                no-exclamation-question-mark = false;
+              };
+              prh = {
+                rulePaths = [
+                  "${pkgs.textlint-rule-prh}/lib/node_modules/textlint-rule-prh/node_modules/prh/prh-rules/media/WEB+DB_PRESS.yml"
+                  "${pkgs.textlint-rule-prh}/lib/node_modules/textlint-rule-prh/node_modules/prh/prh-rules/media/techbooster.yml"
+                ];
+              };
             };
           };
-        };
-      in
-      {
-        formatter = treefmtEval.config.build.wrapper;
-        # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-        # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        in {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              deno.enable = true;
+              rufo.enable = true;
+            };
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            (textlint.withPackages [
-              textlint-rule-preset-ja-technical-writing
-              textlint-rule-prh
-            ])
+            settings.formatter = { };
+          };
 
-            ruby
-            rubyPackages.thor
+          # When execute `nix develop`, you go in shell installed nil.
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              (textlint.withPackages [
+                textlint-rule-preset-ja-technical-writing
+                textlint-rule-prh
+              ])
 
-            nil
-            lua-language-server
-            efm-langserver
-            nodePackages_latest.typescript-language-server
-            ruby-lsp
-            tailwindcss-language-server
+              ruby
+              rubyPackages.thor
 
-            just
-            nushell
-          ];
+              nil
+              lua-language-server
+              efm-langserver
+              nodePackages_latest.typescript-language-server
+              ruby-lsp
+              tailwindcss-language-server
+
+              just
+              nushell
+	      git-secrets
+            ];
 
           shellHook = ''
               unlink .textlintrc
               ln -s ${textlintrc} .textlintrc
           '';
+          };
         };
-      }
-    );
+    };
 }
