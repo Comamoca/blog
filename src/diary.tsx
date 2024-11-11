6b01b3af7f793d3f7e22ece46a83f124549c1344
Comamoca <comamoca.dev@gmail.com>
2024-11-12 05:57:04 +0900

    :gem: (index.tsx, diary.tsx) Improved components
    The process of creating a list of components is common to one component.

--- src/diary.tsx
+++ src/diary.tsx
@@ -1,15 +1,33 @@
 import { basename } from "jsr:@std/path";
+import PostList from "./components/PostList.tsx";
 import Logo from "./components/Logo.tsx";
 
 export const layout = "layouts/main.tsx";
 
 export default async function ({ search }: Lume.Data, helpers: Lume.Helpers) {
+  const pages = search.pages("posts")
+    .filter((page) => basename(page.url).indexOf("-diary") != -1)
+    .sort((a, b) => {
+      // Sort articles by `pubDate`
+
+      const dateA = new Date(a.pubDate);
+      const dateB = new Date(b.pubDate);
+
+      return (dateA - dateB) * -1;
+    });
+
   return (
     <>
-      <div className="-mt-20 mb-10">
-        <Logo />
-        <h2 className="flex justify-center mt-8 text-2xl">すべての日報</h2>
+      <div className="flex justify-center flex-col">
+        <div className="mt-7 mx-auto">
+          <Logo />
+        </div>
+        <h2 className="flex justify-center mt-5 mb-5 text-xl md:text-2xl">
+          すべての日報
+        </h2>
       </div>
+      <div className="mx-8 flex md:items-center flex-col gap-6 grid-cols-4">
+        <PostList pages={pages} isDiary={true} />
       </div>
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,31 @@
 import { basename } from "jsr:@std/path";
 import PostCard from "./components/PostCard.tsx";
+import PostList from "./components/PostList.tsx";
 import Logo from "./components/Logo.tsx";
 
 export const layout = "layouts/main.tsx";
 
 export default async function ({ search }: Lume.Data, helpers: Lume.Helpers) {
+  const pages = search.pages("posts")
+    .filter((page) => basename(page.url).indexOf("-diary") == -1)
+    .sort((a, b) => {
+      // Sort articles by `pubDate`
 
+      const dateA = new Date(a.pubDate);
+      const dateB = new Date(b.pubDate);
 
+      return (dateA - dateB) * -1;
+    });
+
+  return (
+    <>
+      <div className="mx-8 text-lg md:mx-auto">
+        <div className="my-10 flex justify-center">
+          <Logo />
+        </div>
+        <div className="flex md:items-center flex-col gap-6 grid-cols-4">
+          <PostList pages={pages} />
+        </div>
       </div>
     </>
   );
