7f26ae0873a5e95e890c4a986ffe73329ba43239
Comamoca <comamoca.dev@gmail.com>
2024-11-15 01:27:03 +0900

    :fire: (src-old/) Remove backup directory

--- src-old/components/CardList.tsx
+++
@@ -1,5 +0,0 @@
-export default function CardList(props: { posts }) {
-  const posts = props.posts;
-
-  return <div className={"flex flex-col items-center mt-3"}>{posts}</div>;
-}
--- src-old/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Post {
-  slug: string;
-  title: string;
-  publishedAt: Date;
-  content: string;
-  snippet: string;
-  path: string;
-  root: string;
-}
