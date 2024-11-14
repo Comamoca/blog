adb846c8b4f66045fa74aee2e71e0363c3791280
Comamoca <comamoca.dev@gmail.com>
2024-11-15 03:13:52 +0900

    :sparkles: (utils/fetchogp.ts) Update type and logic

--- utils/fetchogp.ts
+++ utils/fetchogp.ts
@@ -1,10 +1,13 @@
 import { DOMParser } from "jsr:@b-fuze/deno-dom";
 import * as v from "npm:valibot";
 import { is } from "jsr:@core/unknownutil";
+import { green, red } from "jsr:@std/fmt/colors";
+import { basename, join } from "jsr:@std/path";
 
 export const OGInfoSchema = v.object({
-  url: v.string(),
-  title: v.string(),
+  url: v.union([v.string(), v.undefined()]),
+  siteTitle: v.string(),
+  title: v.union([v.string(), v.undefined()]),
   image: v.union([v.string(), v.undefined()]),
   name: v.union([v.string(), v.undefined()]),
 });
@@ -16,24 +19,31 @@ export async function fetchOGInfo(
   timeout = 5000,
 ): Promise<OGInfo> {
   const cache = await caches.open("fetchOgp");
+  const _url = new URL(url);
 
   const resp = await (async () => {
     const resp = await cache.match(url);
 
     if (is.Undefined(resp)) {
-      const req = new Request(new URL(url));
+      console.log(red(`=> ${_url.origin}: Unused cache.`));
+
+      const req = new Request(_url);
       const resp = await fetch(req, { signal: AbortSignal.timeout(timeout) });
       await cache.put(req, resp);
 
       return resp;
     } else {
+      console.log(green(`=> ${_url.origin}: Using cache.`));
+
       return resp;
     }
   })();
 
   const body = await resp.text();
   const doc = new DOMParser().parseFromString(body, "text/html");
-  const og = [...doc.querySelectorAll("meta")]
+  const title = doc.title;
+
+  const ogInfo = [...doc.querySelectorAll("meta")]
     .filter((elem) => elem.hasAttribute("property"))
     .map((elem) => {
       if (elem.getAttribute("property") == "og:url") {
@@ -45,16 +55,31 @@ export async function fetchOGInfo(
       }
 
       if (elem.getAttribute("property") == "og:image") {
-        return { image: elem.getAttribute("content") };
+        const imageUrl = elem.getAttribute("content");
+
+        if (is.String(imageUrl)) {
+          const base = basename(imageUrl);
+
+          if (URL.canParse(imageUrl)) {
+            return { image: imageUrl };
+          } else {
+            return { image: join(_url.origin, base) };
+          }
+        } else {
+          return undefined;
+        }
       }
 
       if (elem.getAttribute("property") == "og:site_name") {
         return { name: elem.getAttribute("content") };
       }
-    })
-    .reduce((acc, obj) => {
-      return obj ? { ...acc, ...obj } : acc;
-    }, {});
+    });
+
+  ogInfo.push({ siteTitle: title });
+
+  const og = ogInfo.reduce((acc, obj) => {
+    return obj ? { ...acc, ...obj } : acc;
+  }, {});
 
   return v.parse(OGInfoSchema, og);
 }
