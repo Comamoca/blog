6259eeae1dc154ccda841d3fd139dc46027ae717
Comamoca <comamoca.dev@gmail.com>
2024-11-18 05:06:03 +0900

    :sparkles: (fetch_ogp.ts) Add new flag to disable cache

--- utils/fetchogp.ts
+++ utils/fetchogp.ts
@@ -3,6 +3,7 @@ import * as v from "npm:valibot";
 import { is } from "jsr:@core/unknownutil";
 import { green, red } from "jsr:@std/fmt/colors";
 import { basename, join } from "jsr:@std/path";
+import { logger } from "../utils/logger.ts";
 
 export const OGInfoSchema = v.object({
   url: v.union([v.string(), v.undefined()]),
@@ -18,17 +19,32 @@ export async function fetchOGInfo(
   url: string,
   timeout = 10000,
 ): Promise<OGInfo> {
+  const useCahce = !is.Undefined(Deno.env.get("NO_CACHE"));
+
   const cache = await caches.open("fetchOgp");
   const _url = new URL(url);
 
   const resp = await (async () => {
     const resp = await cache.match(url);
+    const isJSR = _url.origin.indexOf("jsr.io") != -1;
+    const header = {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+    };
 
-    if (is.Undefined(resp)) {
+    if (is.Undefined(resp) || useCahce) {
       console.log(red(`=> ${_url.origin}: Unused cache.`));
 
-      const req = new Request(_url);
-      const resp = await fetch(req, { signal: AbortSignal.timeout(timeout) });
+      const resp = await fetch(_url, {
+        headers: isJSR
+          ? {
+            ...header,
+            "accept":
+              "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+          }
+          : header,
+        signal: AbortSignal.timeout(timeout),
+      });
       await cache.put(req, resp.clone());
 
       return resp;
@@ -75,6 +91,7 @@ export async function fetchOGInfo(
       }
     });
 
+  logger.debug(`[fetchOGInfo] siteTitle: ${title}`);
   ogInfo.push({ siteTitle: title });
 
   const og = ogInfo.reduce((acc, obj) => {
