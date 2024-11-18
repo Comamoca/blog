f30eb8c0751385bb3e300e37140f344b50c73af9
Comamoca <comamoca.dev@gmail.com>
2024-11-18 17:23:49 +0900

    :sparkles: (Twemoji) Update style

--- src/components/Twemoji.tsx
+++ src/components/Twemoji.tsx
@@ -1,12 +1,14 @@
-export default function Twemoji(props) {
+export default function Twemoji({ emoji, size = 20 }) {
   const url = new URL("https://emoji2svg.deno.dev");
-  url.pathname += `api/${props.emoji}`;
+  url.pathname += `api/${emoji}`;
+
+  const style = `size-${size} md:size-${size + 8}`;
 
   return (
     <>
       <img
         src={url.toString()}
-        className="pb-2 size-20 md:size-28"
+        className={style}
       />
     </>
   );
