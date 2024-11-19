be3a8ce7110204ef7320cf1492397573aae7b3e5
Comamoca <comamoca.dev@gmail.com>
2024-11-20 06:16:40 +0900

    :beetle: (layouts/) Update pagefind script

--- src/_includes/layouts/post.tsx
+++ src/_includes/layouts/post.tsx
@@ -56,12 +56,8 @@ export default function (
         {/*HACK: デプロイ時にPagefindのスクリプトが実行されないため追加した*/}
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-const elem = document.getElementById('search')
-
-if (elem) {
-    if (element.children.length === 0) {
-        new PagefindUI({"element":"#search","showImages":false,"excerptLength":0,"showEmptyFilters":true,"showSubResults":false,"resetStyles":true,"bundlePath":"/pagefind/","baseUrl":"/"});}`,
+            __html: `const elem = document.getElementById('search')
+if (elem) {if (elem.children.length === 0) {new PagefindUI({"element":"#search","showImages":false,"excerptLength":0,"showEmptyFilters":true,"showSubResults":false,"resetStyles":true,"bundlePath":"/pagefind/","baseUrl":"/"});}}`,
           }}
         />
       </html>
