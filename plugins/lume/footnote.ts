924b416e72e302a3a49a3ffe066eb0c4117230ac
Comamoca <comamoca.dev@gmail.com>
2024-11-20 12:29:36 +0900

    :tada: (plugins/lume/footnote.ts) Add lume plugin for footnote

---
+++ plugins/lume/footnote.ts
@@ -0,0 +1,47 @@
+import type Site from "lume/core/site.ts";
+import { is } from "jsr:@core/unknownutil";
+
+export default function () {
+  return (site: Site) => {
+    console.log("footer plugin");
+
+    site.process([".html"], (pages) => {
+      for (const page of pages) {
+        const { document } = page;
+        if (!document) {
+          continue;
+        }
+
+        // Remove all styles at footnote label.
+        const footerLabel = document.getElementById("footnote-label");
+        if (!is.Null(footerLabel)) {
+          footerLabel.removeAttribute("class");
+          footerLabel.innerText = "脚注";
+        }
+
+        // Change footnote backref text
+        const footnotesBackref = document.querySelectorAll(
+          "[data-footnote-backref]",
+        );
+        footnotesBackref.forEach((a) => {
+          console.log("==> Change textContent");
+          a.textContent = "↩︎";
+        });
+
+        // Add style to footnote list.
+        const footnotesElements = document.querySelectorAll(".footnotes ol");
+        footnotesElements.forEach((ol) => {
+          const li = ol.querySelectorAll("li");
+          li.forEach((node) => {
+            node.classList.add("!my-1");
+          });
+
+          const p = ol.querySelectorAll("p");
+          p.forEach((node) => {
+            node.classList.add("!my-1");
+          });
+        });
+      }
+    });
+  };
+}
