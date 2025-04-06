f6130f5e9c4180ed1691331cf4f7a3596ef556a1
Comamoca <comamoca.dev@gmail.com>
2025-04-06 16:57:33 +0900

    :package: (flake.nix) Pinned nixpkgs version for wrangler

--- flake.nix
+++ flake.nix
@@ -51,6 +51,12 @@
               stripRoot = false;
             };
 
+            wrangler-pkgs = import (builtins.fetchTarball {
+              url = "https://github.com/NixOS/nixpkgs/archive/21808d22b1cda1898b71cf1a1beb524a97add2c4.tar.gz";
+            }) { };
+
+            wrangler = wrangler-pkgs.wrangler;
+
             buildPhase = ''
               mkdir -p $out/bin
 
