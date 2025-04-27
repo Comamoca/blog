808494d5f3e43b971725f1aa439096d8462529b9
Comamoca <comamoca.dev@gmail.com>
2025-04-28 03:43:32 +0900

    :sparkles: (Header.tsx, Search.tsx) Enable search component

--- src/_components/Search.tsx
+++ src/_components/Search.tsx
@@ -11,11 +11,21 @@ export default function () {
           <Search />
         </div>
       </label>
-      <input type="checkbox" id="pagefind_modal" className="modal-toggle" />
+
+      <input
+        type="checkbox"
+        id="pagefind_modal"
+        className="modal-toggle hidden"
+      />
       <div className="modal modal-bottom sm:modal-middle" role="dialog">
         <div className="modal-box">
           <div className="flex flex-col h-dvh py-5">
             <div id="search" className="mx-3"></div>
+          </div>
+          <div
+            id="search"
+            className="mx-3 sm:mx-6 md:mx-10 lg:mx-20 xl:mx-40"
+          >
           </div>
         </div>
         <label className="modal-backdrop" htmlFor="pagefind_modal">Close</label>
