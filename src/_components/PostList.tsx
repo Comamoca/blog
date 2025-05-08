53db11f3c977fd199406dcdc4b0f22382204b610
Comamoca <comamoca.dev@gmail.com>
2025-05-09 01:38:57 +0900

    :sparkles: (_components/PostList.tsx) Update component

--- src/_components/PostList.tsx
+++ src/_components/PostList.tsx
@@ -1,21 +1,23 @@
 import PostCard from "./PostCard.tsx";
 
-export default ({ pages = [], helpers, isDiary }) => (
-  <>
-    {pages.map((page, idx) => {
-      return (
-        <div
-          key={idx}
-          className="flex flex-grow justify-center md:w-8/12 md:mx-5 "
-        >
-          <PostCard
-            title={page.title}
-            slug={page.url}
-            description={page.description}
-            isDiary={isDiary}
-          />
-        </div>
-      );
-    })}
-  </>
-);
+export default ({ comp, pages = [], helpers, isDiary }) => {
+  return (
+    <>
+      {pages.map((page, idx) => {
+        return (
+          <div
+            key={idx}
+            className="flex flex-grow justify-center md:w-8/12 md:mx-5 "
+          >
+            <PostCard
+              title={page.title}
+              slug={page.url}
+              description={page.description}
+              isDiary={isDiary}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
