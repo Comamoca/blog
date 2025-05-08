adc2444693d42c5d4105f53bce607ba2b1435ba1
Comamoca <comamoca.dev@gmail.com>
2025-05-09 01:39:24 +0900

    :sparkles: (index.tsx) Update index page

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { basename } from "jsr:@std/path";
-import PostCard from "./_components/PostCard.tsx";
 import { SITE_DESCRIPTION, SITE_TITLE } from "./consts.ts";
-import PostList from "./_components/PostList.tsx";
-import Logo from "./_components/Logo.tsx";
+// import PostCard from "./_components/PostCard.tsx";
+// import PostList from "./_components/PostList.tsx";
+// import Logo from "./_components/Logo.tsx";
 
 export const title = SITE_TITLE;
 export const layout = "layouts/main.tsx";
@@ -12,7 +12,7 @@ export const metas = {
   description: SITE_DESCRIPTION,
 };
 
-export default function ({ search }: Lume.Data, helpers: Lume.Helpers) {
+export default function ({ search, comp }: Lume.Data, helpers: Lume.Helpers) {
   const pages = search.pages("posts")
     .filter((page) => basename(page.url).indexOf("-diary") == -1)
     .sort((a, b) => {
@@ -28,11 +28,11 @@ export default function ({ search }: Lume.Data, helpers: Lume.Helpers) {
     <>
       <div className="mx-8 text-lg md:mx-auto h-screen">
         <div className="my-10 flex justify-center">
-          <Logo />
+          <comp.Logo />
         </div>
         <div className="flex md:items-center flex-col gap-6 grid-cols-4">
-          <PostList pages={pages.slice(0, 5)} />
-          <PostCard
+          <comp.PostList pages={pages.slice(0, 5)} />
+          <comp.PostCard
             title="全ての投稿"
             description="全ての投稿はこちらから"
             slug="/all/1"
