e12a2d0ef6f56f186a9dccf0df40a27e33ac3d73
Comamoca <93137338+Comamoca@users.noreply.github.com>
2025-10-06 09:25:10 +0900

    :fire: (me/) Remove script tag

--- src/me.page.tsx
+++ src/me.page.tsx
@@ -31,8 +31,6 @@ export default async function ({ comp }: Lume.Data) {
   return (
     <>
       <comp.BaseHead title={SITE_TITLE} description={SITE_DESCRIPTION} />
-
-      <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"></script>
       <img
         src="https://r2.comamoca.dev/icon.png"
         className="mx-auto h-32 w-32 rounded-full md:mb-6 mt-8 md:mt-2"
