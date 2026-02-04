efa9e484b6774af5a32f4ff7b6386c4dcc287006
Comamoca <comamoca.dev@gmail.com>
2026-02-04 21:45:02 +0900

    feat: (Logo) Remove leading spaces

--- src/_components/Logo.tsx
+++ src/_components/Logo.tsx
@@ -4,7 +4,7 @@ export default async function () {
   return (
     <>
       <div className="flex flex-grow justify-center w-max">
-        <h1 className="text-2xl md:text-4xl px-auto pl-5 mx-auto">
+        <h1 className="text-2xl md:text-4xl px-auto mx-auto">
           {SITE_TITLE}
         </h1>
       </div>
